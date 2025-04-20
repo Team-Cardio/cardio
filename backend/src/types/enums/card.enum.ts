ce935121808b31bbe87f590b5b33526c9a9f670b
Michał Hoffmann <michalzkato@gmail.com>
2025-04-20 15:31:28 +0200

    too lazy to cherrypick

---
+++ backend/src/types/enums/card.enum.ts
@@ -0,0 +1,21 @@
+export enum Color {
+    SPADE,
+    HEART,
+    DIAMOND,
+    CLUB,
+  }
+  export enum Rank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+  }
