e79094918ef95bcbd12306cf0fa798375f1a8504
Michał Hoffmann <michalzkato@gmail.com>
2025-05-04 20:21:23 +0200

    pre merge

--- backend/src/engine/poker/logic/poker-hand-compare.ts
+++ backend/src/engine/poker/logic/poker-hand-compare.ts
@@ -8,8 +8,8 @@ export class PokerHandCompare {
     communityCards: Card[],
     hand2: Card[],
   ): number {
-    const pokerHand1 = PokerHandEval.evaluateHand(hand1, communityCards);
-    const pokerHand2 = PokerHandEval.evaluateHand(hand2, communityCards);
+    const [pokerHand1, playerCards1] = PokerHandEval.evaluateHand(hand1, communityCards);
+    const [pokerHand2, playerCards2] = PokerHandEval.evaluateHand(hand2, communityCards);
 
     if (pokerHand1 > pokerHand2) {
       return 1; // Hand 1 wins
@@ -17,54 +17,16 @@ export class PokerHandCompare {
     if (pokerHand1 < pokerHand2) {
       return -1; // Hand 2 wins
     }
-    const rankCount1 = this.getCardDistribution(hand1, communityCards);
-    const rankCount2 = this.getCardDistribution(hand2, communityCards);
-
-    const ranks1 = Array.from(rankCount1.keys());
-    const ranks2 = Array.from(rankCount2.keys());
-    const counts1 = Array.from(rankCount1.values());
-    const counts2 = Array.from(rankCount2.values());
-
-    // compare the counts first
-    for (let i = 0; i < Math.min(counts1.length, counts2.length); i++) {
-      if (counts1[i] > counts2[i]) {
+    // compare by player cards, already sorted by rank
+    for (let i = 0; i < playerCards1.length; i++) {
+      if (playerCards1[i].rank > playerCards2[i].rank) {
         return 1; // Hand 1 wins
       }
-      if (counts1[i] < counts2[i]) {
-        return -1; // Hand 2 wins
-      }
-    }
-    // If counts are equal, compare the ranks
-    for (let i = 0; i < Math.min(ranks1.length, ranks2.length); i++) {
-      if (ranks1[i] > ranks2[i]) {
-        return 1; // Hand 1 wins
-      }
-      if (ranks1[i] < ranks2[i]) {
+      if (playerCards1[i].rank < playerCards2[i].rank) {
         return -1; // Hand 2 wins
       }
     }
     return 0; // Tie
-  }
-
-  static getCardDistribution(
-    hand: Card[],
-    communityCards: Card[],
-  ): Map<Rank, number> {
-    const allCards = [...hand, ...communityCards];
-    const rankCount = new Map<Rank, number>();
-    for (const card of allCards) {
-      rankCount.set(card.rank, (rankCount.get(card.rank) || 0) + 1);
-    }
-
-    const sortedRankCount = new Map(
-      [...rankCount.entries()].sort((a, b) => {
-        if (a[1] === b[1]) {
-          return b[0] - a[0]; // Sort by rank in descending order if counts are equal
-        }
-        return b[1] - a[1]; // Sort by count in descending order
-      }),
-    );
-
-    return sortedRankCount;
+   
   }
 }
--- backend/src/engine/poker/logic/poker-hand-eval.ts
+++ backend/src/engine/poker/logic/poker-hand-eval.ts
@@ -1,41 +1,54 @@
 import { Card } from 'src/types/card';
-import { Rank } from 'src/types/enums/card.enum';
+import { Color, Rank } from 'src/types/enums/card.enum';
 import { PokerHand } from '../poker-types';
 
 export class PokerHandEval {
-  static evaluateHand(hand: Card[], communityCards: Card[]): PokerHand {
+  static evaluateHand(hand: Card[], communityCards: Card[]): [PokerHand, Card[]] {
     const allCards = [...hand, ...communityCards];
-    const handRank = this.getHandRank(allCards);
-    return handRank;
+    return this.getHandRank(allCards);
   }
 
-  private static getHandRank(cards: Card[]): PokerHand {
+  private static getHandRank(cards: Card[]): [PokerHand, Card[]] {
     const isFlush = this.isFlush(cards);
     const isStraight = this.isStraight(cards);
 
     if (isFlush && isStraight) {
-      if (cards.some((card) => card.rank === Rank.ACE)) {
-        return PokerHand.RoyalFlush;
+      for(const color in Color)
+      {
+        const oneColorCards = cards.filter((card) => card.color === color)
+
       }
-      return PokerHand.StraightFlush;
     }
 
-    const rankCount = new Map<Rank, number>();
+    const rankCount = new Map<Rank, {cnt: number, rank: Rank}>();
     for (const card of cards) {
-      rankCount.set(card.rank, (rankCount.get(card.rank) || 0) + 1);
+      rankCount.set(card.rank, {cnt: (rankCount.get(card.rank)?.cnt || 0 ) + 1, rank: card.rank});
     }
-    const counts = Array.from(rankCount.values()).sort((a, b) => b - a);
-    const isFourOfAKind = counts[0] === 4;
-    const isFullHouse = counts[0] === 3 && counts[1] === 2;
-    const isThreeOfAKind = counts[0] === 3 && counts[1] === 1;
-    const isTwoPair = counts[0] === 2 && counts[1] === 2;
-    const isOnePair = counts[0] === 2 && counts[1] === 1;
+    const counts = Array.from(rankCount.values()).sort((a, b) => {
+      if (a.cnt === b.cnt) {
+        return b.rank - a.rank;
+      }
+      return b.cnt - a.cnt;
+    }
+    );
+
+    const isFourOfAKind = counts[0].cnt === 4;
+    const isFullHouse = counts[0].cnt === 3 && counts[1].cnt >= 2;
+    const isThreeOfAKind = counts[0].cnt === 3 && counts[1].cnt === 1;
+    const isTwoPair = counts[0].cnt === 2 && counts[1].cnt === 2;
+    const isOnePair = counts[0].cnt === 2 && counts[1].cnt === 1;
 
     if (isFourOfAKind) {
-      return PokerHand.FourOfAKind;
+      const four = cards.filter((card) => card.rank === counts[0].rank);
+      const kicker = cards.filter((card) => card.rank !== counts[0].rank).sort(
+        (a, b) => b.rank - a.rank,
+      )[0];
+      return [PokerHand.FourOfAKind, [...four, kicker]];
     }
     if (isFullHouse) {
-      return PokerHand.FullHouse;
+      const three = cards.filter((card) => card.rank === counts[0].rank);
+      const pair = cards.filter((card) => card.rank === counts[1].rank).slice(0, 2); 
+      return [PokerHand.FullHouse, [...three, ...pair]];
     }
     if (isFlush) {
       return PokerHand.Flush;
@@ -44,32 +57,52 @@ export class PokerHandEval {
       return PokerHand.Straight;
     }
     if (isThreeOfAKind) {
-      return PokerHand.ThreeOfAKind;
+      const three = cards.filter((card) => card.rank === counts[0].rank);
+      const kicker1 = cards.filter((card) => card.rank === counts[1].rank);
+      const kicker2 = cards.filter((card) => card.rank === counts[2].rank);
+      return [PokerHand.ThreeOfAKind, [...three, ...kicker1, ...kicker2]];
     }
     if (isTwoPair) {
-      return PokerHand.TwoPair;
+      const pair1 = cards.filter((card) => card.rank === counts[0].rank);
+      const pair2 = cards.filter((card) => card.rank === counts[1].rank);
+      const kicker = cards.filter((card) => card.rank !== counts[0].rank && card.rank !== counts[1].rank).sort(
+        (a, b) => b.rank - a.rank,
+      )[0];
+      return [PokerHand.TwoPair, [...pair1, ...pair2, kicker]];
     }
     if (isOnePair) {
-      return PokerHand.OnePair;
+      const pair = cards.filter((card) => card.rank === counts[0].rank);
+      const kickers = cards.filter((card) => card.rank !== counts[0].rank).sort(
+        (a, b) => b.rank - a.rank,
+      ).slice(0, 3);
+      return [PokerHand.OnePair, [...pair, ...kickers]];
     }
 
-    return PokerHand.HighCard;
+    return [PokerHand.HighCard, cards.sort((a, b) => b.rank - a.rank).slice(0, 5)];
   }
 
   private static isStraight(cards: Card[]): boolean {
     const ranks = cards.map((card) => card.rank).sort((a, b) => a - b);
-    for (let i = 0; i < ranks.length - 1; i++) {
-      if (ranks[i] + 1 !== ranks[i + 1]) {
-        return false;
-      }
-    }
+    const consecutiveCount = ranks.reduce((count, rank, index) => {
+      if (index === 0) return count;
+      return rank === ranks[index - 1] + 1 ? count + 1 : count;
+    }, 1);
+      
     return true;
   }
 
-  private static isFlush(cards: Card[]): boolean {
-    const colorCount = Array.from(
-      new Set(cards.map((card) => card.color)),
-    ).length;
-    return colorCount === 1;
+  private static isFlush(cards: Card[]): [boolean, Color?] {
+    const counts: Record<Color, number> = {
+      [Color.SPADE]: 0,
+      [Color.HEART]: 0,
+      [Color.DIAMOND]: 0,
+      [Color.CLUB]: 0,
+    };
+
+    for (const card of cards) {
+      if (++counts[card.color] >= 5) return [true, card.color]
+    }
+  
+    return [false, undefined]
   }
 }
