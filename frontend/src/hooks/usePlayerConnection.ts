e62692d926ae428fa600287892cc00fcaba5007e
Adam Szwaja <szwajaadam@gmail.com>
2025-06-15 00:36:14 +0200

    Add async storage with player id

--- frontend/src/hooks/usePlayerConnection.ts
+++ frontend/src/hooks/usePlayerConnection.ts
@@ -1,10 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { PlayerAction, PlayerPayload, PlayerRoomData } from "../types/RoomData";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function usePlayerConnection(code: string) {
   const wsRef = useRef<Socket>();
-  const [playerId, setPlayerId] = useState<string>();
+  const [playerId, setPlayerId] = useState<string>("");
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [roomData, setRoomData] = useState<PlayerRoomData>({
     playerID: "",
     isMyTurn: true,
@@ -15,7 +17,36 @@ export function usePlayerConnection(code: string) {
     cards: [],
   });
 
+  const savePlayerId = async (playerId: string) => {
+    try {
+      await AsyncStorage.setItem('playerId', JSON.stringify({ code, playerId }));
+    } catch (e) {
+      console.error('Failed to save data', e);
+    }
+  };
+
+  const loadPlayerId = async () => {
+    try {
+      const value = await AsyncStorage.getItem('playerId');
+      if (value !== null) {
+        const parsedValue = JSON.parse(value);
+        if (code === parsedValue.code) {
+          setPlayerId(String(parsedValue.playerId));
+        }
+      }
+      setIsLoaded(true);
+    } catch (e) {
+      console.error('Failed to load data', e);
+    }
+  };
+
+  useEffect(() => { loadPlayerId() }, []);
+
   useEffect(() => {
+    if (!isLoaded) {
+      return;
+    }
+
     const ws = io("http://localhost:3000/ws/game", {
       transports: ["websocket"],
       path: "/socket.io",
@@ -23,9 +54,15 @@ export function usePlayerConnection(code: string) {
     wsRef.current = ws;
 
     ws.on("connect", () => {
-      console.log("[WebSocket] Connected");
+      console.log(`[WebSocket] Connected ${playerId}`);
 
-      ws.emit("join-room", { code });
+      if (playerId) {
+        console.log(`join with playerId ${playerId}`)
+        ws.emit("join-room", { code, playerId });
+      } else {
+        console.log(`joined without playerId ${playerId}`)
+        ws.emit("join-room", { code });
+      }
     });
 
     ws.on("disconnect", () => {
@@ -37,7 +74,8 @@ export function usePlayerConnection(code: string) {
     });
 
     ws.on("joined", (data: PlayerPayload) => {
-      setPlayerId(data.payload.playerID);
+      setPlayerId(String(data.payload.playerID));
+      savePlayerId(String(data.payload.playerID));
     });
 
     ws.on("update-room", (data: PlayerPayload) => {
@@ -47,14 +85,15 @@ export function usePlayerConnection(code: string) {
     return () => {
       ws.disconnect();
     };
-  }, [code]);
+  }, [code, isLoaded]);
 
   const emitPlayerAction = (action: PlayerAction) => {
     if (playerId == undefined) {
       console.error("[WebSocket] Player ID is undefined");
       return;
     }
-    wsRef.current?.emit("action", {playerId, action});
+    console.log(action, playerId)
+    wsRef.current?.emit("action", { playerId, action });
   };
 
   const disconnect = () => {
