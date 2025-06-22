b9a8ac642830f5434b629b9999db220aa4221d84
Michał Hoffmann <michalzkato@gmail.com>
2025-06-22 17:39:36 +0200

    vm setup changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RoomModule } from './room/room.module';
 
+const passwd = process.env.REDIS_PASSWD;
+const port = process.env.REDIS_PORT;
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     RedisModule.forRoot({
       type: 'single',
-      url: 'redis://localhost:' + (process.env.REDIS_PORT ?? '6379'),
+      url: `redis://:${passwd}@redis:${port}`,
     }),
     RoomModule,
   ],
